e02f8a1b826029bdd36fbfaec8e778df81421fab
Marcelo Souto <marcelosouto676@gmail.com>
2023-02-02 21:57:00 -0300

    page switch with correct pages

--- src/pages/PageSwitcher.jsx
+++ src/pages/PageSwitcher.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import DatabaseTotal from './DatabaseTotal';
 import TheGreatestStatistics from './TheGreatestStatistics';
 import FiftyGreatestGames from './FiftyGreatestGames';
+import TheBestGames from './TheBestGames';
+import CompaniesStatistics from './CompaniesStatistics';
 
 function PageSwitcher({ page }) {
-  
   const pages = {
     1: <DatabaseTotal />,
-    2: null,
+    2: <CompaniesStatistics />,
     3: <TheGreatestStatistics />,
-    4: null,
+    4: <TheBestGames />,
     5: <FiftyGreatestGames />,
   };
 
