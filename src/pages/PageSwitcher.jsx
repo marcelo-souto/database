93a5144866ae5a1f42c19d5e3e1120c2a4ad12ba
Marcelo Souto <marcelosouto676@gmail.com>
2023-02-04 17:48:37 -0300

    "merge"

--- src/pages/PageSwitcher.jsx
+++ src/pages/PageSwitcher.jsx
@@ -19,31 +19,3 @@ function PageSwitcher({ page }) {
 
   return pages[page];
 }
-
-export default PageSwitcher;
-import React from 'react'
-import Loading from '../components/helper/Loading';
-const DatabaseTotal = React.lazy(() => import('./DatabaseTotal'));
-const TheGreatestStatistics = React.lazy(() => import('./TheGreatestStatistics'));
-const FiftyGreatestGames = React.lazy(() => import('./FiftyGreatestGames'));
-const TheBestGames = React.lazy(() => import('./TheBestGames'));
-const CompaniesStatistics = React.lazy(() => import('./CompaniesStatistics'));
-
-
-function PageSwitcher({ page }) {
-  const pages = {
-    1: <DatabaseTotal />,
-    2: <CompaniesStatistics />,
-    3: <TheGreatestStatistics />,
-    4: <TheBestGames />,
-    5: <FiftyGreatestGames />,
-  };
-
-  return (
-    <React.Suspense fallback={<Loading />}>
-      {pages[page]}
-    </React.Suspense>
-  );
-}
-
-export default PageSwitcher;
